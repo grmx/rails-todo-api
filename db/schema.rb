d56a4ac57b6228fe5cc5fb408b51ccfac5cf42c7
Max Grechko <gremax@gremax.me>
2017-11-22 18:40:41 +0200

    Add Comment model
    - Create model
    - Add validations
    - Add associations

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171118160032) do
+ActiveRecord::Schema.define(version: 20171122163154) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.bigint "task_id"
+    t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["task_id"], name: "index_comments_on_task_id"
+  end
 
   create_table "projects", force: :cascade do |t|
     t.bigint "user_id"
@@ -59,6 +67,7 @@ ActiveRecord::Schema.define(version: 20171118160032) do
     t.index ["username"], name: "index_users_on_username", unique: true
   end
 
+  add_foreign_key "comments", "tasks"
   add_foreign_key "projects", "users"
   add_foreign_key "tasks", "projects"
 end
