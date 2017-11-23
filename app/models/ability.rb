f2829bfe0ac570135e154d63c0d1bf85a158b46b
Max Grechko <gremax@gremax.me>
2017-11-23 19:41:11 +0200

    Add authorization for Comment

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -8,5 +8,6 @@ class Ability
 
     can :manage, Project, user_id: user.id
     can :manage, Task, project: { user_id: user.id }
+    can :manage, Comment, task: { project: { user_id: user.id } }
   end
 end
