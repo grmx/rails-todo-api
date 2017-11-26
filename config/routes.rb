08a4df1aced83855abfe710a4330be55dd92209b
Max Grechko <gremax@gremax.me>
2017-11-26 16:29:38 +0200

    Add CommentsController

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,9 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :projects do
-        resources :tasks
+        resources :tasks, shallow: true do
+          resources :comments
+        end
       end
     end
   end
